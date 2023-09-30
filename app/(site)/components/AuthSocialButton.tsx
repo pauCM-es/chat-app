5a5bbb075d672137053eebe538105761e4aa3cf3
pauCM-es <costamingarro@gmail.com>
2023-09-30 21:42:45 +0200

    feat: add login page and form

---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,37 @@
+import { IconType } from 'react-icons'
+
+import Button from '@/app/components/Button'
+import React from 'react'
+
+interface AuthSocialButtonProps {
+  icon: IconType
+  onClick: () => void
+}
+
+const AuthSocialButton = ({ icon: Icon, onClick }: AuthSocialButtonProps) => {
+  return (
+    <button
+      type='button'
+      onClick={ onClick }
+      className='
+      inline-flex
+      w-full
+      justify-center
+      rounded-md
+      bg-white
+      px-4
+      py-2
+      text-gray-500
+      shadow-sm
+      ring-1
+      ring-inset
+      hover:bg-gray-50
+      focus:outline-offset-0
+    '
+    >
+      <Icon />
+    </button>
+  )
+}
+
+export default AuthSocialButton
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,18 +1,43 @@
+import clsx from 'clsx'
 import React, { ReactNode } from 'react'
 
 interface ButtonProps {
   type?: 'button' | 'submit' | 'reset'
-  fullWidth: boolean
   children: ReactNode
+  fullWidth?: boolean
   onClick?: () => void
   secondary?: boolean
   danger?: boolean
   disable?: boolean
 }
 
-const Button = () => {
+const Button = ({ type, fullWidth = false, children, onClick, secondary, danger, disable }: ButtonProps) => {
   return (
-    <div>Button</div>
+    <button
+      onClick={ onClick }
+      type={ type }
+      disabled={ disable }
+      className={ clsx(`
+    clex
+    justify-center
+    rounded-md
+    px-3
+    py-2
+    text-sm
+    font-semibold
+    focus-visible:outline
+    focus-visible:outline-2
+    focus-visible:outline-offset-2
+    `,
+        disable && "opacity-50 cursor-default",
+        fullWidth && "w-full",
+        secondary ? "text-gray-900" : "text-white",
+        danger && "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
+        !secondary && !danger && "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
+      ) }
+    >
+      { children }
+    </button>
   )
 }
 
